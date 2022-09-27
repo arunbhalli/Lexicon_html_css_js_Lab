436b021b1a13b0a17873e7aa393bec963c2f33b3
arunbhalli <tomarbhalli@gmail.com>
2022-09-27 18:27:50 +0200

    adds solution for filter age

---
+++ lab5/filterbyage.js
@@ -0,0 +1,9 @@
+
+    function filterByAge(arr1) {
+      var obj = {
+        name: arr1[1],
+        age: arr1[0]
+      }
+      console.log(obj)
+    }
+    filterByAge(['12', 'John', '15', 'Alfred', '9'])
