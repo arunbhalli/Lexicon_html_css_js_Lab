c6a10081783de74952dfc485893f0f30d8a3b4f9
arunbhalli <tomarbhalli@gmail.com>
2022-09-27 18:26:14 +0200

    adds solution for distance between points

---
+++ lab5/distanBTpoint.js
@@ -0,0 +1,11 @@
+function distance(arr) {
+  let x1 = arr[0];
+  let y1 = arr[1];
+  let x2 = arr[2];
+  let y2 = arr[3];
+  let a1 = x2 - x1;
+  let a2 = y2 - y1;
+  let distance = a1 * a1 + a2 * a2;
+  console.log('Distance between points is '+ ' '+Math.sqrt(distance));
+}
+distance(['6', '6', '3', '3']);
