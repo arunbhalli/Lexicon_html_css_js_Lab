380f098c808d8a33bb6d449984d6b7909ca2e19f
arunbhalli <tomarbhalli@gmail.com>
2022-10-04 21:35:01 +0200

    modified button selected functionality

--- lab8/q4/button.js
+++ lab8/q4/button.js
@@ -1,14 +1,8 @@
 $(document).ready(function () {
-  $('#check1 ').click(function () {
-    $('#check1').addClass('selected');
-    // $('.button').css('background-color', 'blue')
-  });
-  $('#check2 ').click(function () {
-    $('#check2').addClass('selected');
-    // $('.button').css('background-color', 'blue')
-  });
-  $('#check3 ').click(function () {
-    $('#check3').addClass('selected');
-    // $('.button').css('background-color', 'blue')
-  });
+  $('a.button').on('click', buttonClicked);
+
+  function buttonClicked() {
+      $('.selected').removeClass('selected');
+      $(this).addClass('selected');
+  }
 });
