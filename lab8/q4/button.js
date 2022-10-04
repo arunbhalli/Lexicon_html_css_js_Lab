8756015bf7504fc42b3ddb721525e4b2f8de8a44
arunbhalli <tomarbhalli@gmail.com>
2022-10-04 15:30:13 +0200

    adds functionality for selected button in a link tag

---
+++ lab8/q4/button.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  $('#check1 ').click(function () {
+    $('#check1').addClass('selected');
+    // $('.button').css('background-color', 'blue')
+  });
+  $('#check2 ').click(function () {
+    $('#check2').addClass('selected');
+    // $('.button').css('background-color', 'blue')
+  });
+  $('#check3 ').click(function () {
+    $('#check3').addClass('selected');
+    // $('.button').css('background-color', 'blue')
+  });
+});
